2cc40efa741763fb71a8e9dda029d1e101049235
Sbakken21 <sbakken21@gmail.com>
2017-11-06 23:38:47 -0600

    prod build

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,11 +4,11 @@ class Results extends Component {
     render(){
         let percent = (this.props.score / this.props.questions.length * 100).toFixed(2);
         if(percent > 80){
-            let message = 'Good Job!';
+            var message = 'Good Job!';
         } else if(percent < 80 && percent > 60){
-            let message = 'Not Bad';
+            var message = 'Not Bad';
         } else {
-            let message = 'Try Again';
+            var message = 'Try Again';
         }
         return(
             <div>
